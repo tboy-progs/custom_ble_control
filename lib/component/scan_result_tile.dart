0095810dda68557e7c544f3e0fbec67c1dbe0725
taka <gluestick829@gmail.com>
2024-10-15 22:50:48 +0900

    Add:scan result tileを新規作成

---
+++ lib/component/scan_result_tile.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ScanResultTile extends StatefulWidget {
+  const ScanResultTile({super.key});
+
+  @override
+  State<ScanResultTile> createState() => _ScanResultTileState();
+}
+
+class _ScanResultTileState extends State<ScanResultTile> {
+  @override
+  Widget build(BuildContext context) {
+    return Placeholder();
+  }
+}
